60773e515096e0e2e7b66c609189759b659c347a
Karl Newell <karlnewell@gmail.com>
2018-10-03 12:06:16 -0700

    Update requirements.txt and bgp_yang.py

--- bgp_yang.py
+++ bgp_yang.py
@@ -1,33 +1,41 @@
-import napalm_yang
+from builtins import input
+from consolemenu import ConsoleMenu, SelectionMenu
+from consolemenu.items import FunctionItem, SubmenuItem, CommandItem
 import json
+import napalm_yang
 import yaml
 
 def pretty_print(dictionary):
     print(json.dumps(dictionary, sort_keys=True, indent=4))
 
-config = napalm_yang.base.Root()
-
-config.add_model(napalm_yang.models.openconfig_interfaces())
-
 with open("intf.yml") as f:
     stream = f.read()
     temp = yaml.load(stream)
 
-raw_input("View the YAML data model.  Press Enter")
-print(stream)
-
-raw_input("View the data model as a python dict.  Press Enter")
-pretty_print(temp)
-
-raw_input("View the YANG model.  Warning: long output. Press Enter")
-pretty_print(napalm_yang.utils.model_to_dict(config))
-
-raw_input("View the generated YANG data.  Press Enter")
+config = napalm_yang.base.Root()
+config.add_model(napalm_yang.models.openconfig_interfaces())
 config.load_dict(temp)
-pretty_print(config.get(filter=True))
 
-raw_input("View JunOS config.  Press Enter")
-print(config.translate_config(profile=["junos"]))
+def show_data(args):
+    if "yaml" in args:
+        print(stream)
+    elif "dict" in args:
+        pretty_print(temp)
+    elif "yang" in args:
+        pretty_print(napalm_yang.utils.model_to_dict(config))
+    elif "gen" in args:
+        pretty_print(config.get(filter=True))
+    elif "junos" in args:
+        print(config.translate_config(profile=["junos"]))
+    elif "ios" in args:
+        print(config.translate_config(profile=["ios"]))
+    else:
+        return
+    input("Press Enter")
 
-raw_input("View IOS config.  Press Enter")
-print(config.translate_config(profile=["ios"]))
+menu = ConsoleMenu("Menu")
+menu_dict = {"yaml":"YAML data model", "dict": "python dictionary", "yang": "YANG model.  Warning: long output.", "gen": "generated YANG data", "junos":"JunOS config", "ios":"IOS config"}
+for key, value in menu_dict.items():
+    function_item = FunctionItem("View the " + value, show_data, [key])
+    menu.append_item(function_item)
+menu.show()
